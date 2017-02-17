eb64082dacd652815f7697e6c9690c29c488e757
gauravd-mobicules <gaurav.dixit@mobicules.com>
2017-02-17 15:02:40 +0530

    worked on order and notification adapter

---
+++ app/src/main/java/com/e2b/model/response/Order.java
@@ -0,0 +1,45 @@
+package com.e2b.model.response;
+
+/**
+ * Created by gaurav on 17/2/17.
+ */
+
+public class Order {
+
+    private String id;
+    private String title;
+    private String date;
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
