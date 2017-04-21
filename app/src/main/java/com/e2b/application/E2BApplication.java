b4ca768052eb0c1813637bc05d953778f3789d48
gauravd-mobicules <gaurav.dixit@mobicules.com>
2017-04-21 10:32:35 +0530

    worked on merchant

--- app/src/main/java/com/e2b/application/E2BApplication.java
+++ app/src/main/java/com/e2b/application/E2BApplication.java
@@ -2,11 +2,9 @@ package com.e2b.application;
 
 import android.app.Application;
 import android.content.Context;
-
-import com.e2b.api.ApiClient;
-import com.e2b.api.IApiRequest;
+import e2bmerchant.api.ApiClient;
+import e2bmerchant.api.IApiRequest;
 import e2bmerchant.utils.MerchantPreferenceKeeper;
-
 
 /**
  * This is the application class.
@@ -20,7 +18,6 @@ public class E2BApplication extends Application {
         super.onCreate();
         context = this;
         MerchantPreferenceKeeper.setContext(getApplicationContext());
-
         ApiClient.init(IApiRequest.class);
     }
 }
